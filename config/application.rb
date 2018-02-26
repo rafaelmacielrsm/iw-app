7e0080befaaee5a3245aa2324d35e057df42cc5b
Rafael Maciel <rafaelmacielrsm@gmail.com>
2018-02-25 22:18:30 -0300

    Creates a new landing page
    - Adds a new home controller and implements a landing page to the app.
    - Adds many assets files ranging from stylesheets to images.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module IwApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.view_specs    false
+      g.helper_specs  false
+      g.stylesheets   false
+      g.javascripts   false
+      g.helper        false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
